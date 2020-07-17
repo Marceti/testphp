1d9d82017d7df3cd12e49f9c61823093e477e34f
Marceti <marceti@gmail.com>
2020-07-17 09:22:15 +0300

    added projects.index basic

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Project;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', function(){
+   return Project::all();
+})->name('project.index');
